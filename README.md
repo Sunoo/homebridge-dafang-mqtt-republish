8a5cc459f6bd06ff486ac586de9037680a2ef9e3
David Maher <sunookitsune@kitsunet.org>
2020-07-24 22:08:31 -0400

    Added support for publishing accessories

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # homebridge-dafang-mqtt-republish
+
 [![npm](https://img.shields.io/npm/v/homebridge-dafang-mqtt-republish) ![npm](https://img.shields.io/npm/dt/homebridge-dafang-mqtt-republish)](https://www.npmjs.com/package/homebridge-dafang-mqtt-republish)
 
 This plugin republishes MQTT motion messages from [Dafang Hacks](https://github.com/EliasKotlyar/Xiaomi-Dafang-Hacks) into a format [homebridge-camera-ffmpeg](https://github.com/homebridge-plugins/homebridge-camera-ffmpeg) understands.
 
-Note that this plugin itself does not expose any devices to HomeKit.
+## Installation
 
-### Installation
 > More detailed setup instructions available [in the wiki](https://github.com/Sunoo/homebridge-dafang-mqtt-republish/wiki/Configuring-Dafang-Hacks-Cameras-for-Homebridge).
 
 1. Install Homebridge using the [official instructions](https://github.com/homebridge/homebridge/wiki).
@@ -14,31 +14,46 @@ Note that this plugin itself does not expose any devices to HomeKit.
 4. Update your configuration file. See configuration sample below.
 
 ### Configuration
+
 Edit your `config.json` accordingly. Configuration sample:
- ```
-    "platforms": [
-        {
-            "platform": "dafangMqtt",
-            "server": "10.0.1.190",
-            "port": 1883,
-            "homebridge_topic": "homebridge/motion",
-            "cameras": [
-                {
-                    "name": "Cat Food Camera",
-                    "dafang_topic": "dafang/catcam"
-                }
-            ]
-        }
-    ]
+
+ ```json
+"platforms": [
+    {
+        "platform": "dafangMqtt",
+        "server": "10.0.1.190",
+        "port": 1883,
+        "homebridge_topic": "homebridge",
+        "cameras": [
+            {
+                "name": "Cat Food Camera",
+                "dafang_topic": "dafang/catcam"
+            }
+        ]
+    }
+]
 ```
 
-| Fields               | Description                                                                             | Required |
-|----------------------|-----------------------------------------------------------------------------------------|----------|
-| platform             | Must always be `dafangMqtt`.                                                            | Yes      |
-| server               | The address of your MQTT server. (Default: 127.0.0.1)                                   | No       |
-| port                 | The port of your MQTT server. (Default: 1883)                                           | No       |
-| homebridge_topic     | MQTT topic that homebridge-camera-ffmpeg is subscribed to. (Default: homebridge/motion) | Yes      |
-| cameras              | Array of Dafang Hacks camera configs (multiple supported).                              | Yes      |
-| \|- name             | Name of your camera. (Needs to be the same as in homebridge-camera-ffmpeg config)       | Yes      |
-| \|- dafang_topic     | MQTT topic that your camera publishes to. (Must be unique per camera)                   | Yes      |
-| \|- cooldown         | Cooldown in seconds. Set to 0 to disable.                                               | No       |
+- `server`: The address of your MQTT server. (Default: 127.0.0.1)
+- `port`: The port of your MQTT server. (Default: 1883)
+- `homebridge_topic`: MQTT topic that homebridge-camera-ffmpeg is subscribed to. If not set, motion alerts will not be passed on.
+- `cameras`: _(Required)_ Array of Dafang Hacks camera configs (multiple supported).
+  - `name`: _(Required)_ Name of your camera. (Needs to be the same as in homebridge-camera-ffmpeg config)
+  - `dafang_topic`: _(Required)_ MQTT topic that your camera publishes to. (Must be unique per camera)
+  - `cooldown`: Motion cooldown in seconds. Set to 0 to disable.
+  - `manufacturer`: Manufacturer for exposed accessories.
+  - `model`: Model for exposed accessories.
+  - `serialNumber`: Serial number for exposed accessories.
+  - `firmwareRevision`: Firmware revision for exposed accessories.
+  - `accessories`
+    - `blueLed`: Exposes blue LED as a lightbulb.
+    - `yellowLed`: Exposes yellow LED as a lightbulb.
+    - `irLed`: Exposes IR LED as a lightbulb.
+    - `irCut`: Exposes IR filter as a switch.
+    - `brightness`: Exposes brightness as a light sensor. Can be set to either `hw` or `virtual` and should match what your camera is configured to use. `virtual` is experimental and the values are unlikely to be accurate or useful.
+    - `nightMode`: Exposes night mode as a switch.
+    - `motionTracking`: Exposes motion tracking as a switch.
+    - `motorsVertical`: Exposes tilting up and down as switches.
+    - `motorsHorizontal`: Exposes panning left and right as switches.
+    - `recording`: Exposes video recording as a switch.
+    - `snapshot`: Exposes snapshot as a switch.
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const mqtt = require('mqtt');
+const version = require('./package.json').version;
+
+var Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function(homebridge) {
+    Accessory = homebridge.platformAccessory;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
     homebridge.registerPlatform('homebridge-dafang-mqtt-republish', 'dafangMqtt', dafangMqtt, true);
 }
 
 function dafangMqtt(log, config, api) {
     this.log = log;
+    this.config = config;
+    this.api = api;
+
+    this.accessories = [];
 
     const server = config.server || '127.0.0.1';
     const port = config.port || '1883';
     this.mqttUrl = 'mqtt://' + server + ':' + port;
 
-    this.homebridge_topic = 'homebridge';
-    if (config.homebridge_topic && config.homebridge_topic != 'homebridge/motion') {
-      mqtttopic = config.homebridge_topic;
+    if (config.homebridge_topic) {
+        if(config.homebridge_topic == 'homebridge/motion') {
+            this.homebridge_topic = 'homebridge';
+        } else {
+            this.homebridge_topic = config.homebridge_topic;
+        }
     }
 
     const cameraConfigs = config.cameras || [];
@@ -61,7 +75,255 @@ function dafangMqtt(log, config, api) {
         }
     });
 
-    api.on('didFinishLaunching', this.connectMqtt.bind(this));
+    api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
+}
+
+dafangMqtt.prototype.configureAccessory = function(accessory) {
+    this.log('Configuring accessories for ' + accessory.displayName + '...');
+
+    const config = this.config.cameras.find((camera) => camera.name === accessory.displayName);
+
+    if (!config || !config.accessories) {
+        this.accessories.push(accessory);
+        return;
+    }
+
+    const cameraAccessoryInfo = accessory.getService(Service.AccessoryInformation);
+    if (cameraAccessoryInfo) {
+        cameraAccessoryInfo.updateCharacteristic(Characteristic.Manufacturer, config.manufacturer || 'Dafang');
+        cameraAccessoryInfo.updateCharacteristic(Characteristic.Model, config.model || 'Hacks');
+        cameraAccessoryInfo.updateCharacteristic(Characteristic.SerialNumber, config.serialNumber || 'SerialNumber');
+        cameraAccessoryInfo.updateCharacteristic(Characteristic.FirmwareRevision, config.firmwareRevision || version);
+    }
+
+    let blueLedService = accessory.getServiceById(Service.Lightbulb, '/leds/blue');
+    let yellowLedService = accessory.getServiceById(Service.Lightbulb, '/leds/yellow');
+    let irLedService = accessory.getServiceById(Service.Lightbulb, '/leds/ir');
+    let irCutService = accessory.getServiceById(Service.Switch, '/ir_cut');
+    let brightnessService = accessory.getServiceById(Service.LightSensor, '/brightness');
+    let nightModeService = accessory.getServiceById(Service.Switch, '/night_mode');
+    let motionTrackingService = accessory.getServiceById(Service.Switch, '/motion/tracking');
+    let motorsUpService = accessory.getServiceById(Service.Switch, '/motors/vertical/up');
+    let motorsDownService = accessory.getServiceById(Service.Switch, '/motors/vertical/down');
+    let motorsLeftService = accessory.getServiceById(Service.Switch, '/motors/horizontal/left');
+    let motorsRightService = accessory.getServiceById(Service.Switch, '/motors/horizontal/right');
+    let recordingService = accessory.getServiceById(Service.Switch, '/recording');
+    let snapshotService = accessory.getServiceById(Service.Switch, '/snapshot');
+
+    if (blueLedService) {
+        accessory.removeService(blueLedService);
+    }
+    if (yellowLedService) {
+        accessory.removeService(yellowLedService);
+    }
+    if (irLedService) {
+        accessory.removeService(irLedService);
+    }
+    if (irCutService) {
+        accessory.removeService(irCutService);
+    }
+    if (brightnessService) {
+        accessory.removeService(brightnessService);
+    }
+    if (nightModeService) {
+        accessory.removeService(nightModeService);
+    }
+    if (motionTrackingService) {
+        accessory.removeService(motionTrackingService);
+    }
+    if (motorsUpService) {
+        accessory.removeService(motorsUpService);
+    }
+    if (motorsDownService) {
+        accessory.removeService(motorsDownService);
+    }
+    if (motorsLeftService) {
+        accessory.removeService(motorsLeftService);
+    }
+    if (motorsRightService) {
+        accessory.removeService(motorsRightService);
+    }
+    if (recordingService) {
+        accessory.removeService(recordingService);
+    }
+    if (snapshotService) {
+        accessory.removeService(snapshotService);
+    }
+
+    if (config.accessories.blueLed) {
+        blueLedService = new Service.Lightbulb(config.name + ' Blue LED', '/leds/blue');
+        blueLedService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/leds/blue/set', message);
+            callback();
+        });
+        accessory.addService(blueLedService);
+    }
+    if (config.accessories.yellowLed) {
+        yellowLedService = new Service.Lightbulb(config.name + ' Yellow LED', '/leds/yellow');
+        yellowLedService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/leds/yellow/set', message);
+            callback();
+        });
+        accessory.addService(yellowLedService);
+    }
+    if (config.accessories.irLed) {
+        irLedService = new Service.Lightbulb(config.name + ' IR LED', '/leds/ir');
+        irLedService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/leds/ir/set', message);
+            callback();
+        });
+        accessory.addService(irLedService);
+    }
+    if (config.accessories.irCut) {
+        irCutService = new Service.Switch(config.name + ' IR Filter', '/ir_cut');
+        irCutService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/ir_cut/set', message);
+            callback();
+        });
+        accessory.addService(irCutService);
+    }
+    if (config.accessories.brightness) {
+        brightnessService = new Service.LightSensor(config.name + ' Brightness', '/brightness');
+        accessory.addService(brightnessService);
+    }
+    if (config.accessories.nightMode) {
+        nightModeService = new Service.Switch(config.name + ' Night Mode', '/night_mode');
+        nightModeService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/night_mode/set', message);
+            callback();
+        });
+        accessory.addService(nightModeService);
+    }
+    if (config.accessories.motionTracking) {
+        motionTrackingService = new Service.Switch(config.name + ' Motion Tracking', '/motion/tracking');
+        motionTrackingService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/motion/tracking/set', message);
+            callback();
+        });
+        accessory.addService(motionTrackingService);
+    }
+    if (config.accessories.motorsVertical) {
+        motorsUpService = new Service.Switch(config.name + ' Tilt Up', '/motors/vertical/up');
+        motorsUpService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            if (value) {
+                this.publishMqtt(config.dafang_topic + '/motors/vertical/set', 'up');
+                if (value) {
+                    setTimeout(() => {
+                        motorsUpService.updateCharacteristic(Characteristic.On, false);
+                    }, 1000);
+                }
+            }
+            callback();
+        });
+        accessory.addService(motorsUpService);
+        motorsDownService = new Service.Switch(config.name + ' Tilt Down', '/motors/vertical/down');
+        motorsDownService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            if (value) {
+                this.publishMqtt(config.dafang_topic + '/motors/vertical/set', 'down');
+                if (value) {
+                    setTimeout(() => {
+                        motorsDownService.updateCharacteristic(Characteristic.On, false);
+                    }, 1000);
+                }
+            }
+            callback();
+        });
+        accessory.addService(motorsDownService);
+    }
+    if (config.accessories.motorsHorizontal) {
+        motorsLeftService = new Service.Switch(config.name + ' Pan Left', '/motors/horizontal/left');
+        motorsLeftService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            if (value) {
+                this.publishMqtt(config.dafang_topic + '/motors/horizontal/set', 'left');
+                if (value) {
+                    setTimeout(() => {
+                        motorsLeftService.updateCharacteristic(Characteristic.On, false);
+                    }, 1000);
+                }
+            }
+            callback();
+        });
+        accessory.addService(motorsLeftService);
+        motorsRightService = new Service.Switch(config.name + ' Pan Right', '/motors/horizontal/right');
+        motorsRightService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            if (value) {
+                this.publishMqtt(config.dafang_topic + '/motors/horizontal/set', 'right');
+                if (value) {
+                    setTimeout(() => {
+                        motorsRightService.updateCharacteristic(Characteristic.On, false);
+                    }, 1000);
+                }
+            }
+            callback();
+        });
+        accessory.addService(motorsRightService);
+    }
+    if (config.accessories.recording) {
+        recordingService = new Service.Switch(config.name + ' Recording', '/recording');
+        recordingService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            const message = value ? 'ON' : 'OFF';
+            this.publishMqtt(config.dafang_topic + '/recording/set', message);
+            callback();
+        });
+        accessory.addService(recordingService);
+    }
+    if (config.accessories.snapshot) {
+        snapshotService = new Service.Switch(config.name + ' Snapshot', '/snapshot');
+        snapshotService.getCharacteristic(Characteristic.On)
+        .on('set', (value, callback) => {
+            if (value) {
+                this.publishMqtt(config.dafang_topic + '/snapshot/set', 'ON');
+                if (value) {
+                    setTimeout(() => {
+                        snapshotService.updateCharacteristic(Characteristic.On, false);
+                    }, 1000);
+                }
+            }
+            callback();
+        });
+        accessory.addService(snapshotService);
+    }
+
+    this.accessories.push(accessory);
+}
+
+dafangMqtt.prototype.didFinishLaunching = function() {
+    this.config.cameras.forEach((camera) => {
+        if (camera.accessories) {
+            if (!this.accessories.find((acc) => acc.displayName === camera.name)) {
+                const uuid = UUIDGen.generate(camera.dafang_topic);
+                const accessory = new Accessory(camera.name, uuid);
+                this.configureAccessory(accessory);
+                this.api.registerPlatformAccessories('homebridge-dafang-mqtt-republish', 'dafangMqtt', [accessory]);
+            }
+        }
+      });
+  
+      this.accessories.forEach((accessory) => {
+        const config = this.config.cameras.find((camera) => camera.name === accessory.displayName)
+        if (!config || !config.accessories) {
+          this.api.unregisterPlatformAccessories('homebridge-dafang-mqtt-republish', 'dafangMqtt', [accessory]);
+        }
+      });
+
+    this.connectMqtt();
 }
 
 dafangMqtt.prototype.publishMqtt = function(topic, message) {
@@ -69,7 +331,7 @@ dafangMqtt.prototype.publishMqtt = function(topic, message) {
     this.client.publish(topic, message);
 }
 
-dafangMqtt.prototype.parseMsg = function(message) {
+dafangMqtt.prototype.parseBoolMsg = function(message) {
     switch (message) {
         case 'ON':
             return true;
@@ -80,34 +342,80 @@ dafangMqtt.prototype.parseMsg = function(message) {
     }
 }
 
-dafangMqtt.prototype.handleMotion = function(camera, bool) {
-    if (bool) {
-        camera.motion = true;
-        if (!camera.timer) {
-            this.publishMqtt(this.homebridge_topic + '/motion', camera.name);
-        } else {
-            this.log.debug('Motion set received, but cooldown running: ' + camera.name);
+dafangMqtt.prototype.handleMotion = function(camera, message) {
+    if (this.homebridge_topic) {
+        const isOn = this.parseBoolMsg(message);
+        if (isOn === undefined) {
+            return;
         }
-        if (camera.cooldown > 0) {
-            if (camera.timer) {
-                this.log.debug('Cancelling existing cooldown timer: ' + camera.name);
-                clearTimeout(camera.timer);
+        if (isOn) {
+            camera.motion = true;
+            if (!camera.timer) {
+                this.publishMqtt(this.homebridge_topic + '/motion', camera.name);
+            } else {
+                this.log.debug('Motion set received, but cooldown running: ' + camera.name);
             }
-            this.log.debug('Cooldown enabled, starting timer: ' + camera.name);
-            camera.timer = setTimeout(function() {
-                this.log.debug('Cooldown finished, motion detected = ' + camera.motion + ': ' + camera.name);
-                if (!camera.motion) {
-                    this.publishMqtt(this.homebridge_topic + '/motion/reset', camera.name);
+            if (camera.cooldown > 0) {
+                if (camera.timer) {
+                    this.log.debug('Cancelling existing cooldown timer: ' + camera.name);
+                    clearTimeout(camera.timer);
                 }
-                camera.timer = null;
-            }.bind(this), camera.cooldown * 1000);
-        }
-    } else {
-        camera.motion = false;
-        if (!camera.timer) {
-            this.publishMqtt(this.homebridge_topic + '/motion/reset', camera.name);
+                this.log.debug('Cooldown enabled, starting timer: ' + camera.name);
+                camera.timer = setTimeout(function() {
+                    this.log.debug('Cooldown finished, motion detected = ' + camera.motion + ': ' + camera.name);
+                    if (!camera.motion) {
+                        this.publishMqtt(this.homebridge_topic + '/motion/reset', camera.name);
+                    }
+                    camera.timer = null;
+                }.bind(this), camera.cooldown * 1000);
+            }
         } else {
-            this.log.debug('Motion clear received, but cooldown running: ' + camera.name);
+            camera.motion = false;
+            if (!camera.timer) {
+                this.publishMqtt(this.homebridge_topic + '/motion/reset', camera.name);
+            } else {
+                this.log.debug('Motion clear received, but cooldown running: ' + camera.name);
+            }
+        }
+    }
+}
+
+dafangMqtt.prototype.getService = function(accessory, type, subtype) {
+    if (accessory) {
+        return accessory.getServiceById(type, subtype);
+    } else {
+        return undefined;
+    }
+}
+
+dafangMqtt.prototype.handleBoolService = function(accessory, type, subtype, message) {
+    const service = this.getService(accessory, type, subtype);
+    if (service) {
+        const isOn = this.parseBoolMsg(message);
+        if (isOn !== undefined) {
+            service.updateCharacteristic(Characteristic.On, isOn);
+        }
+    }
+}
+
+dafangMqtt.prototype.handleBrightness = function(accessory, message) {
+    const value = Number.parseInt(message);
+    if (Number.isNaN(value)) {
+        return;
+    }
+    const service = this.getService(accessory, Service.LightSensor, '/brightness');
+    if (service) {
+        const config = this.config.cameras.find((camera) => camera.name === accessory.displayName);
+        if (config && config.accessories && config.accessories.brightness)
+        {
+            switch (config.accessories.brightness) {
+                case 'hw':
+                    service.updateCharacteristic(Characteristic.CurrentAmbientLightLevel, value);
+                    break;
+                case 'virtual':
+                    service.updateCharacteristic(Characteristic.CurrentAmbientLightLevel, -0.0001 * value + 140);
+                    break;
+            }
         }
     }
 }
@@ -116,25 +424,44 @@ dafangMqtt.prototype.connectMqtt = function() {
     this.client = mqtt.connect(this.mqttUrl);
     this.client.on('connect', () => {
         this.log('MQTT Connection Opened');
-        this.log.debug('Clearing motion for all cameras...');
         this.cameras.forEach(camera => {
             this.client.subscribe(camera.dafang_topic + '/#');
-            this.publishMqtt(this.homebridge_topic + '/reset', camera.name);
         });
     });
     this.client.on('message', (topic, message) => {
         const msg = message.toString();
-        this.log.debug('Received MQTT Message - ' + topic + ': ' + msg);
         const camera = this.cameras.find(camera => topic.startsWith(camera.dafang_topic));
+        const accessory = this.accessories.find((acc) => acc.displayName === camera.name)
         if (camera) {
-            const command = topic.substring(camera.dafang_topic.length + 1);
-            const bool = this.parseMsg(msg);
-            if (bool !== undefined) {
-                switch (command) {
-                    case 'motion':
-                        this.handleMotion(camera, bool);
-                        break;
-                }
+            const command = topic.substring(camera.dafang_topic.length);
+            switch (command) {
+                case '/motion':
+                    this.handleMotion(camera, message);
+                    break;
+                case '/leds/blue':
+                    this.handleBoolService(accessory, Service.Lightbulb, '/leds/blue', msg);
+                    break;
+                case '/leds/yellow':
+                    this.handleBoolService(accessory, Service.Lightbulb, '/leds/yellow', msg);
+                    break;
+                case '/leds/ir':
+                    this.handleBoolService(accessory, Service.Lightbulb, '/leds/ir', msg);
+                    break;
+                case '/ir_cut':
+                    this.handleBoolService(accessory, Service.Switch, '/ir_cut', msg);
+                    break;
+                case '/brightness':
+                    this.handleBrightness(accessory, msg);
+                    break;
+                case '/night_mode':
+                    this.handleBoolService(accessory, Service.Switch, '/night_mode', msg);
+                    break;
+                case '/motion/tracking':
+                    this.handleBoolService(accessory, Service.Switch, '/motion/tracking', msg);
+                    break;
+                case '/recording':
+                    this.handleBoolService(accessory, Service.Switch, '/recording', msg);
+                    break;
             }
         }
     });
