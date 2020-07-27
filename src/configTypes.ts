7fb21eea980705407994d850e436ee9d7e8a02ee
David Maher <sunookitsune@kitsunet.org>
2020-07-26 20:44:19 -0400

    Convert to TypeScript

---
+++ src/configTypes.ts
@@ -0,0 +1,32 @@
+export type DafangMqttPlatformConfig = {
+  name: string;
+  server: string;
+  port: number;
+  homebridge_topic: string;
+  cameras: Array<CameraConfig>;
+};
+
+export type CameraConfig = {
+  name: string;
+  dafang_topic: string;
+  cooldown: number;
+  manufacturer: string;
+  model: string;
+  serialNumber: string;
+  firmwareRevision: string;
+  accessories: AccessoriesConfig;
+};
+
+export type AccessoriesConfig = {
+  blueLed: boolean;
+  yellowLed: boolean;
+  irLed: boolean;
+  irCut: boolean;
+  nightMode: boolean;
+  motionTracking: boolean;
+  motorsVertical: boolean;
+  motorsHorizontal: boolean;
+  recording: boolean;
+  snapshot: boolean;
+  brightness: string;
+};
